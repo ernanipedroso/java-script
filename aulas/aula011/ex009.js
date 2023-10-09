c3a5440a984b909c42e114de73b2ddc921a4e3d6
ernanipedroso <114417688+ernanipedroso@users.noreply.github.com>
2023-10-09 12:17:31 -0300

    novo exercicio

---
+++ aulas/aula011/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'china' 
+if (pais =='brasil') {
+    console.log(`voce e brasileiro seja bem vindo`)
+} else {
+    console.log(`you are a foreigner, wellcome!`)
+}
+if (pais == 'china') {
+    console.log('voce chingchang, no tem galantia pega seu pastel flango')
+}
---
+++ aulas/aula011/exe008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`a veloxidade do seu caro e de ${vel} hm/h`)
+if(vel > 60){
+    console.log(`voce ultrapassou a velocidaded permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o sinto de segurança`)
