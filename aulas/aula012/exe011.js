0529f63aed6fc44671116dfc8042b7d09475f031
ernanipedroso <114417688+ernanipedroso@users.noreply.github.com>
2023-10-10 11:55:46 -0300

    exe de switch e condiçao aninhada

---
+++ aulas/aula012/exe011.js
@@ -0,0 +1,13 @@
+var idade = 67
+if( idade < 16 ){
+
+    console.log ('menor de idade')
+
+} else if ( idade < 18 || idade > 65 ){
+
+    console.log ('voto opcional')
+
+} else{
+
+    console.log('voto obrigatorio')
+}
