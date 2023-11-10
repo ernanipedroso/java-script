06510032280e8b2c88502e6562b36f93fa4e9764
ernanipedroso <114417688+ernanipedroso@users.noreply.github.com>
2023-11-09 23:04:57 -0300

    exercicio tabuada

--- aulas/aula018/exe017/scripy.js
+++ aulas/aula018/exe017/scripy.js
@@ -1,25 +1,20 @@
-function carregar(){
-    var msg =window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date ()
-    var hora = data.getHours()
-    msg.innerHTML= `agora são ${hora} horas`
-    var hora = 18
-   
-   
-
-    if(hora >= 0 && hora <=12){
-        //BOM DIA 
-        img.src = 'foto-manha.jpg'
-        document.body.style.background = '#f9d3af '
-    }else if (hora > 12 && hora < 18){
-        //BOA TARDE 
-        img.src = 'foto-tarde.jpg'
-        document.body.style.background = '#b9846f'
+function tabuada(){
+    let num = window.document.getElementById('txtn')
+    let tab = window.document.getElementById('seltab')
+    
+    if (num.value.length == 0){
+       window.alert('Por favor, digite um numero')
     }else{
-        //BOA NOITE
-        img.src = 'noite-foto.jpg'
-        document.body.style.background = '#6d3816'
-    }
+        var n = Number(num.value)
+        var c = 1 
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = window.document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
 
+    }
 }
